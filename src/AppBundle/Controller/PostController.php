8e1a2c9d41556a3913d1ce810c8ccd0168cd8724
Valik Grinevich <valik.v1per@gmail.com>
2015-01-15 19:03:28 +0200

    add method for get all posts

--- src/AppBundle/Controller/PostController.php
+++ src/AppBundle/Controller/PostController.php
@@ -1,10 +1,22 @@
 <?php
 namespace AppBundle\Controller;
 
+use FOS\RestBundle\Controller\Annotations as Rest;
 use FOS\RestBundle\Controller\FOSRestController;
-use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class PostController extends FOSRestController
 {
+    /**
+     * @return \AppBundle\Entity\Post[]|array
+     *
+     * @Rest\View(templateVar="posts")
+     */
+    public function getPostsAction()
+    {
+        $posts = $this->getDoctrine()
+                    ->getRepository('AppBundle:Post')
+                    ->findAll();
 
+        return $posts;
+    }
 }
