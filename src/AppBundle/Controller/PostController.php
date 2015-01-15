535eb10deb5ce9316c5f52b9eab4902b8596a3aa
Valik Grinevich <valik.v1per@gmail.com>
2015-01-15 18:03:03 +0200

    add postController and routing for him

---
+++ src/AppBundle/Controller/PostController.php
@@ -0,0 +1,10 @@
+<?php
+namespace AppBundle\Controller;
+
+use FOS\RestBundle\Controller\FOSRestController;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class PostController extends FOSRestController
+{
+
+}
